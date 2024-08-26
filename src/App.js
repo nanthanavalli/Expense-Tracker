680858d6440cacf59120cf4af4ae281049f301a0
nanthanavalli <nanthanavalli0309@gmail.com>
2024-08-26 15:44:24 +0530

    Expense tracker with a expense item component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <h2>
+        <ExpenseItem></ExpenseItem>
+      </h2>
+    </div>
+  );
+}
+
+export default App;
