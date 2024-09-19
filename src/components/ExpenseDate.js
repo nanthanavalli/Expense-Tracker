5d210331a778c89af3ebace50ef1f6ee10da26c0
nanthanavalli <nanthanavalli0309@gmail.com>
2024-09-19 22:36:15 +0530

    A custom wrapper component is added

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,4 +1,4 @@
-
+import Card from "./Card";
 import "./ExpenseDate.css";
 
 function ExpenseDate(props) {
@@ -6,11 +6,11 @@ function ExpenseDate(props) {
   const day = props.date.toLocaleString("en-IN", { day: "2-digit" });
   const year = props.date.getFullYear();
   return (
-    <div className="expense-date">
+    <Card className="expense-date">
       <div className="expense-date__month">{month}</div>
       <div className="expense-date__year">{year}</div>
       <div className="expense-date__day">{day}</div>
-    </div>
+    </Card>
   );
 }
 
